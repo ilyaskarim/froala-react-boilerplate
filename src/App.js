ba6b7dd035461ee34c9af4cce50a8059bb3f131a
IlyasKarim <ilyas.datoo@gmail.com>
2019-02-26 14:16:06 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import jquery from "jquery";
 
 import 'froala-editor/js/froala_editor.pkgd.min.js';
 
-// Require Editor CSS files.
 import 'froala-editor/css/froala_style.min.css';
-import 'froala-editor/css/froala_editor.pkgd.min.css';
 
-// Require Font Awesome.
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import FroalaEditor from 'react-froala-wysiwyg';
 import 'font-awesome/css/font-awesome.css';
 
-import FroalaEditor from 'react-froala-wysiwyg';
+window.$ = jquery;
 
-import $ from 'jquery';
-window.$ = $;
+class App extends React.Component {
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <FroalaEditor tag='textarea' config={{
-          toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineClass', 'inlineStyle', 'paragraphStyle', 'lineHeight', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'embedly', 'insertFile', 'insertTable', '|', 'emoticons', 'fontAwesome', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'getPDF', 'spellChecker', 'help', 'html', '|', 'undo', 'redo']
-        }} />
-      </div>
-    );
-  }
+    render() {
+        
+        return (
+          <div>
+           <FroalaEditor
+              tag='textarea'
+              config={{
+                pluginsEnabled: ["align", "charCounter", "codeBeautifier", "codeView", "colors", "draggable", "embedly", "emoticons", "entities", "file", "fontAwesome", "fontFamily", "fontSize", "fullscreen", "image", "imageTUI", "imageManager", "inlineStyle", "inlineClass", "lineBreaker", "lineHeight", "link", "lists", "paragraphFormat", "paragraphStyle", "quickInsert", "quote", "save", "table", "url", "video", "wordPaste"],
+                toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineClass', 'inlineStyle', 'paragraphStyle', 'lineHeight', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'embedly', 'insertFile', 'insertTable', '|', 'emoticons', 'fontAwesome', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'getPDF', 'spellChecker', 'help', 'html', '|', 'undo', 'redo']
+              }}
+            />
+          </div>
+
+      );
+    }
 }
+
 
 export default App;
