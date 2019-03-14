63f86bd09774be916deb166555764554b6a2bef4
IlyasKarim <ilyas.datoo@gmail.com>
2019-03-14 17:17:02 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -16,43 +16,32 @@ import $ from 'jquery';
 window.$ = $;
 
 class App extends React.Component {
-  componentDidMount() {}
-  showMore() {
-    var number = [];
-    var numbertable = [];
-
-    for (var i = 0; i <= 15; i++) {
-      var nUrl = "http://www.arabnews.com/tags/saudi-aramco";
-      var nTitle =
-        "Sudi Aramco Energy Ventures Invests in Norwegian Artificial ";
-      var nsource = "google search";
-      var nDate = "2019-02-25";
-      number.push(
-        "<tr><td><a href =" +
-          nUrl +
-          " target=" +
-          "_blank" +
-          ">" +
-          nTitle +
-          "</a></td></tr>"
-      );
-
-      number.push("<tr><td>" + nsource + " - " + nDate + "</td></tr>");
-
-      number.push("<tr><td></td></tr>");
+  constructor(props) {
+    super(props);
+    this.state=  {
+      boolean: true
     }
-    numbertable.push("<table><tbody>" + number.join(" ") + "</tblody></table>");
-    var contentDiv = document.getElementsByClassName("fr-element fr-view");
-    contentDiv[0].innerHTML = numbertable;
+    this.toggle(true);
   }
-
+  componentDidMount() {}
+  toggle(boolean) {
+    this.setState({
+      boolean: boolean
+    })
+    if (boolean) {
+      // edit.on
+    }else {
+      // edit.off
+    }
+  }
   render() {
     return (
       <div>
-        <div id="froala-editor" />
-        <FroalaEditor tag="textarea" config={{toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineClass', 'inlineStyle', 'paragraphStyle', 'lineHeight', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'embedly', 'insertFile', 'insertTable', '|', 'emoticons', 'fontAwesome', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'getPDF', 'spellChecker', 'help', 'html', '|', 'undo', 'redo'] }} />
+        <div id="froala-editor" >
+          <FroalaEditor tag="textarea" config={{toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineClass', 'inlineStyle', 'paragraphStyle', 'lineHeight', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'embedly', 'insertFile', 'insertTable', '|', 'emoticons', 'fontAwesome', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'getPDF', 'spellChecker', 'help', 'html', '|', 'undo', 'redo'] }} />
+        </div>
 
-        <button type="button" onClick={this.showMore}>
+        <button type="button" onClick={() => this.toggle(!this.state.boolean) }>
           click me
         </button>
       </div>
