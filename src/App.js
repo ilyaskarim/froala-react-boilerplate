c6caf8478c0d85558326972e4a13b1cf65b236bd
IlyasKarim <ilyas.datoo@gmail.com>
2019-04-08 15:17:57 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -19,20 +19,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state=  {
-      boolean: true
+      boolean: true,
+      editor: null
     }
     this.toggle(true);
+    this.toggle = this.toggle.bind(this);
+    this.handleManualController = this.handleManualController.bind(this);
   }
   toggle(boolean) {
     this.setState({
       boolean: boolean
+    })
+    if (boolean) {
+      $(this.state.editor).froalaEditor("edit.on");
+    }else {
+      $(this.state.editor).froalaEditor("edit.off");
+    }
+  }
+
+  handleManualController(e) {
+    e.initialize();
+    this.setState({
+      editor: e.getEditor()(),
     })
   }
 
   render() {
     return (
       <div>
-        <div id="froala-editor" disabled={this.state.boolean} >
+        <div id="froala-editor">
           <FroalaEditor onManualControllerReady={this.handleManualController} tag="textarea" config={{toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineClass', 'inlineStyle', 'paragraphStyle', 'lineHeight', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'embedly', 'insertFile', 'insertTable', '|', 'emoticons', 'fontAwesome', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'getPDF', 'spellChecker', 'help', 'html', '|', 'undo', 'redo'] }} />
         </div>
 
